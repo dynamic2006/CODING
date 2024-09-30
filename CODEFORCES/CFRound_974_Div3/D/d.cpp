754696d43fc6c09771da591178ebdec4eccd5b0f
tejasvi <tejasvi.aynor@gmail.com>
2024-09-30 08:31:52 -0700

    CFRound 974 Div3D upsolved

--- CODEFORCES/CFRound_974_Div3/D/d.cpp
+++ CODEFORCES/CFRound_974_Div3/D/d.cpp
@@ -9,34 +9,27 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
 
     int t; cin >> t;
     while(t--){
         int n, d, k; cin >> n >> d >> k;
-        vector<int> jobDiffTable(n+d+2, 0);
+        //each day from 1 -> n-d+1 can be start day
+        //so job from l to r affects start days l-d+1 to r
+        vector<int> diffTable(n+2, 0);
         for(int i=0; i<k; i++){
             int l, r; cin >> l >> r;
-            jobDiffTable[l]++;
-            jobDiffTable[r+d+1]--;
+            diffTable[max(1, l-d+1)]++;
+            diffTable[r+1]--;
         }
 
-        int minDay = 1, maxDay = 1;
-        int minCount = jobDiffTable[1], maxCount = jobDiffTable[1];
-        vector<int> jobCount(n+d+2, 0);
-        for(int i=1; i<n+d+1; i++){
-            jobCount[i] = jobCount[i-1] + jobDiffTable[i];
-            cout << jobCount[i] << " ";
-            if(jobCount[i] < minCount){
-                minDay = max(1, i-d+1);
-                minCount = jobCount[i];
-            }
-            if(jobCount[i] > maxCount){
-                maxDay = max(1, i-d+1);
-                maxCount = jobCount[i];
-            }
+        int maxDay = 1, minDay = 1;
+        vector<int> prefixSum(n+2, 0);
+        for(int i=1; i<=n-d+1; i++){
+            prefixSum[i] = prefixSum[i-1] + diffTable[i];
+            if(prefixSum[i] > prefixSum[maxDay]) maxDay = i;
+            if(prefixSum[i] < prefixSum[minDay]) minDay = i;
         }
-        cout << endl;
         cout << maxDay << " " << minDay << endl;
     }
 }
