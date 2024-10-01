b34989f426daa4ac16974c9fda049727a20b8aae
tejasvi <tejasvi.aynor@gmail.com>
2024-10-01 08:42:46 -0700

    Upsolved EDU 169 div2 p.D

--- CODEFORCES/EDU_CFRound_169_Div2/D/d.cpp
+++ CODEFORCES/EDU_CFRound_169_Div2/D/d.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 typedef long long ll;
-const ll MAXNUM = 1e18;
+const int MAXNUM = 1e9;
 
 int main() {
     ios::sync_with_stdio(0);
@@ -12,49 +12,67 @@ int main() {
 
     // freopen("d.in", "r", stdin);
 
-    ll t; cin >> t;
+    int t; cin >> t;
     while(t--){
-        ll n, q; cin >> n >> q;
-        vector<string> cityPortals(n+1);
-        map<string, vector<ll>> portalCities;
-        for(ll i=1; i<=n; i++){
-            cin >> cityPortals[i];
-            portalCities[cityPortals[i]].push_back(i);
+        int n, q; cin >> n >> q;
+        vector<string> portals(n);
+        for(int i=0; i<n; i++) cin >> portals[i];
+        vector<vector<pair<int, int>>> closestPortals(n, vector<pair<int, int>>(6));
+
+        vector<string> idxMapPortal = {"BG", "BR", "BY", "GR", "GY", "RY"};
+        map<string, int> portalMapIdx;
+        portalMapIdx["BG"] = 0;
+        portalMapIdx["BR"] = 1;
+        portalMapIdx["BY"] = 2;
+        portalMapIdx["GR"] = 3;
+        portalMapIdx["GY"] = 4;
+        portalMapIdx["RY"] = 5;
+
+        vector<int> nearestPortalIdx(6, -1);
+        for(int i=0; i<n; i++){
+            nearestPortalIdx[portalMapIdx[portals[i]]] = i;
+            for(int j=0; j<6; j++){
+                closestPortals[i][j].first = nearestPortalIdx[j];
+            }
         }
 
-        // return 0;
-
-        for(ll qry=0; qry<q; qry++){
-            ll x, y; cin >> x >> y;
-            char c1 = cityPortals[x][0], c2 = cityPortals[x][1];
-            if(cityPortals[y][0] == c1 || cityPortals[y][0] == c2 || cityPortals[y][1] == c1 || cityPortals[y][1] == c2){
-                cout << abs(x-y) << endl;
-                continue;
+        nearestPortalIdx = vector<int>(6, -1);
+        for(int i=n-1; i>=0; i--){
+            nearestPortalIdx[portalMapIdx[portals[i]]] = i;
+            for(int j=0; j<6; j++){
+                closestPortals[i][j].second = nearestPortalIdx[j];
             }
-
-            // cout << "CHECK" << endl;
-
-            ll ans = MAXNUM;
-            ll a = min(x,y), b = max(x,y);
-            for(auto p : portalCities){
-                if(p.first == cityPortals[a] || p.first == cityPortals[b] || p.second.empty()) continue;
-                auto ptr = lower_bound(p.second.begin(), p.second.end(), a);
-                if(ptr != p.second.end()){
-                    ans = min(ans, abs(*ptr - a) + abs(*ptr - b));
-                    if(*ptr > b && ptr != p.second.begin()){
-                        ans = min(ans, abs(*prev(ptr) - a) + abs(*prev(ptr) - b));
-                    }
-                }
-                else{
-                    ans = min(ans, abs(*prev(p.second.end()) - a) + abs(*prev(p.second.end()) - b));
-                }
-            }
-
-            if(ans == MAXNUM){
-                cout << -1 << endl;
-                continue;
-            }
-            cout << ans << endl;
         }
+
+        for(int x=0; x<q; x++){
+            int a, b; cin >> a >> b;
+            if(a>b) swap(a,b);
+            a--; b--;
+            int minDist = MAXNUM;
+
+            if(portals[a][0] == portals[b][0] || portals[a][0] == portals[b][1] ||
+            portals[a][1] == portals[b][0] || portals[a][1] == portals[b][1]){
+                cout << b-a << endl;
+                continue;
+            }
+            
+            //test valid left 6 nearest portals for b
+            for(int j=0; j<6; j++){
+                if(idxMapPortal[j][0] != portals[b][0] && idxMapPortal[j][0] != portals[b][1] &&
+                idxMapPortal[j][1] != portals[b][0] && idxMapPortal[j][1] != portals[b][1]) continue;
+                else if(idxMapPortal[j] == portals[b] || closestPortals[b][j].first == -1) continue;
+                else minDist = min(minDist, b-closestPortals[b][j].first + abs(closestPortals[b][j].first - a));
+            }
+            //test valid right 6 nearest portals for a
+            for(int j=0; j<6; j++){
+                if(idxMapPortal[j][0] != portals[a][0] && idxMapPortal[j][0] != portals[a][1] &&
+                idxMapPortal[j][1] != portals[a][0] && idxMapPortal[j][1] != portals[a][1]) continue;
+                else if(idxMapPortal[j] == portals[a] || closestPortals[a][j].second == -1) continue;
+                else minDist = min(minDist, closestPortals[a][j].second-a + abs(closestPortals[a][j].second - b));
+            }
+            if(minDist == MAXNUM) cout << -1 << endl;
+            else cout << minDist << endl;
+        }
+
     }
 }
