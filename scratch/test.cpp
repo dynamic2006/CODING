b1daa0e06e7fed2c96d38aa2f4c9c3af72fee2e3
tejasvi <tejasvi.aynor@gmail.com>
2024-10-12 10:41:13 -0700

    cs31 hw

--- scratch/test.cpp
+++ scratch/test.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+    int codeSection;
+    cin >> codeSection;
 
-    cout << "WORKING" << endl;
-
+    switch(codeSection){
+        case 281:
+            cout << "bigamy";
+            break;
+        case 321:
+        case 322:
+            cout << "selling illegal lottery tickets";
+            break;
+        case 383:
+            cout << "selling rancid butter";
+            break;
+        case 598:
+            cout << "injuring a bird in a public cemetery";
+            break;
+        default:
+            cout << "some other crime";
+    }
 }
