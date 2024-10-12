c06d3e539fda747fca1dce76dde03fb44c9ed115
tejasvi <tejasvi.aynor@gmail.com>
2024-10-11 21:32:32 -0700

    icpc prac

--- ICPC_2024_Tryouts/F/f.cpp
+++ ICPC_2024_Tryouts/F/f.cpp
@@ -9,8 +9,28 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
 
     double x_1, y_1, x_2, y_2, a;
+    cin >> x_1 >> y_1 >> x_2 >> y_2 >> a;
+
+    double angle = atan2((y_2 - y_1), (x_2 - x_1));
+    double centerX = (x_1 + x_2)/2;
+    double centerY = (y_1 + y_2)/2;
+
+    a/=2;
+    double c = sqrt((x_1-x_2)*(x_1-x_2) + (y_1-y_2)*(y_1-y_2)) / 2;
+    double b = sqrt(a*a - c*c);
+
+    double deltaX = sqrt(a*a*cos(angle)*cos(angle) + b*b*sin(angle)*sin(angle));
+    double deltaY = sqrt(a*a*sin(angle)*sin(angle) + b*b*cos(angle)*cos(angle));
+
+    double xLow = centerX - deltaX;
+    double xHigh = centerX + deltaX;
+    double yLow = centerY - deltaY;
+    double yHigh = centerY + deltaY;
+
+    cout << setprecision(10) << xLow << " " << yLow << " " << xHigh << " " << yHigh << endl;
     
 }
+
--- ICPC_2024_Tryouts/J/j.cpp
+++ ICPC_2024_Tryouts/J/j.cpp
@@ -9,5 +9,33 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
+
+    int L, xNum, xDen, yNum, yDen, zNum, zDen;
+    cin >> L >> xNum >> xDen >> yNum >> yDen >> zNum >> zDen;
+
+    if(L == 0){
+        cout << 1 << endl;
+        return 0;
+    }
+
+    for(int i=1; i<=L; i++){
+        int count = 0;
+        double xFrac = (double)xNum / xDen;
+        double yFrac = (double)yNum / yDen;
+        double zFrac = (double)zNum / zDen;
+
+        if((double)1/3 < xFrac && xFrac < (double)2/3) count++;
+        if((double)1/3 < yFrac && yFrac < (double)2/3) count++;
+        if((double)1/3 < zFrac && zFrac < (double)2/3) count++;
+        if(count >= 2){
+            cout << 0 << endl;
+            return 0;
+        }
+
+        xNum*=3; xNum %= xDen;
+        yNum*=3; yNum %= yDen;
+        zNum*=3; zNum %= zDen;
+    }
+    cout << 1 << endl;
 }
