3bc6bfa4590526a8a792546a7509e3192a28cff7
tejasvi <tejasvi.aynor@gmail.com>
2024-10-14 14:49:31 -0700

    ran rental prog

--- CS31/HW2/rental.cpp
+++ CS31/HW2/rental.cpp
@@ -2,14 +2,7 @@
 
 using namespace std;
 
-typedef long long ll;
-
 int main() {
-    // ios::sync_with_stdio(0);
-    // cin.tie(0);
-    // cout.tie(0);
-
-    //freopen("input.in", "r", stdin);
 
     int odoStart, odoEnd, rentDays, startMonth;
     string custName, lux;
