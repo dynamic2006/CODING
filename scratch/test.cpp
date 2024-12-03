595e622bd0f094bd3ecc4c1aa8b48ec1a565402c
tejasvi <tejasvi.aynor@gmail.com>
2024-12-02 22:03:39 -0800

    python set up

--- scratch/test.cpp
+++ scratch/test.cpp
@@ -1,22 +1,44 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-void removeS(char* str)
-{
-    char* temp = str;
-    while(*str != 0){
-        if(*str != 'S' && *str != 's'){
-            *temp = *str;
-            temp++;
-        }
-        str++;
-    }
-    *temp = '\0';
-}
+typedef long long ll;
 
-int main()
-{
-    char msg[50] = "She'll blossom like a massless princess.";
-    removeS(msg);
-    cout << msg;  // prints   he'll bloom like a male prince.
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    freopen("input.in", "r", stdin);
+
+    string s;
+    ll total = 0;
+    ll count = 0;
+    while(getline(cin, s)){
+        cout << s << endl;
+        for(int i=0; i<s.size()-2; i++){
+            if(s[i] == 'm' && s[i+1] == 'u' && s[i+2] == 'l'){
+                bool found = false;
+                for(int a = 0; a<=999; a++){
+                    for(int b=0; b<=999; b++){
+                        string aStr = to_string(a), bStr = to_string(b);
+                        string test = "mul(" + aStr + ","  + bStr + ")";
+                        string cur = "";
+                        for(int x=i; x<min(s.size(), i+test.size()); x++) cur += s[x];
+                        if(cur == test){
+                            total += a*b;
+                            found = true;
+                            i += test.size();
+                            cout << count << " FOUND" << endl;
+                            count++;
+                        }
+                        if(found) break;
+                    }
+                    if(found) break;
+                }
+            }
+        }
+        cout << "FINISHED LINE" << endl;
+    }
+    cout << total << endl;
 }
