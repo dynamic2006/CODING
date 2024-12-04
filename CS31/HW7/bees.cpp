f6bf0a596bbed1e96112b870809127ae232577aa
tejasvi <tejasvi.aynor@gmail.com>
2024-12-04 10:55:25 -0800

    testing

--- CS31/HW7/bees.cpp
+++ CS31/HW7/bees.cpp
@@ -744,12 +744,12 @@ void doBasicTests()
 
 int main()
 {
-    doBasicTests(); // Remove this line after completing test.
-    return 0;       // Remove this line after completing test.
+    // doBasicTests(); // Remove this line after completing test.
+    // return 0;       // Remove this line after completing test.
       // Create a game
       // Use this instead to create a mini-game:   Game g(3, 4, 2);
-    // Game g(7, 8, 25);
-    Game g(3,4,2);
+    Game g(7, 8, 25);
+    // Game g(3,4,2);
 
       // Play the game
     g.play();
