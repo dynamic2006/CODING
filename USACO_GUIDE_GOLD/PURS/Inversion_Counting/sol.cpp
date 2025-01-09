65f1cb1008cfbc43f584486df01b1c760868e2fc
tejasvi <tejasvi.aynor@gmail.com>
2025-01-08 18:42:02 -0800

    Inversion Counting USACO Guide Gold

--- USACO_GUIDE_GOLD/PURS/Inversion_Counting/sol.cpp
+++ USACO_GUIDE_GOLD/PURS/Inversion_Counting/sol.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
+#include <bits/extc++.h>
+using namespace __gnu_pbds;
+
+template<class T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 typedef long long ll;
 
@@ -9,7 +14,18 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
 
-    
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        ll ans = 0;
+        Tree<int> t;
+        for(int i=0; i<n; i++){
+            int a; cin >> a;
+            t.insert(a);
+            ans += i - t.order_of_key(a);
+        }
+        cout << ans << endl;
+    }
 }
