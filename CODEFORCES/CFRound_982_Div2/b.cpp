d0ffc67f07a5404182ea0ca5d4cdccb6e30e84af
tejasvi <tejasvi.aynor@gmail.com>
2025-01-16 10:50:20 -0800

    upsolved b

--- CODEFORCES/CFRound_982_Div2/b.cpp
+++ CODEFORCES/CFRound_982_Div2/b.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include <bits/extc++.h>
 using namespace __gnu_pbds;
 
-using Tree = tree<pair<int, int> , null_type, less_equal<pair<int, int>> , rb_tree_tag, tree_order_statistics_node_update>;
+using Tree = tree<int , null_type, less_equal<int> , rb_tree_tag, tree_order_statistics_node_update>;
 
 typedef long long ll;
 
@@ -13,7 +13,7 @@ int main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("input.in", "r", stdin);
+    // freopen("input.in", "r", stdin);
 
     int t; cin >> t;
     while(t--){
@@ -24,18 +24,13 @@ int main() {
         Tree tree;
         vector<int> complement(n, 0);
         for(int i=n-1; i>=0; i--){
-            tree.insert({a[i], i});
-            complement[i] = tree.order_of_key({a[i], i});
+            tree.insert(a[i]);
+            complement[i] = tree.order_of_key(a[i]+1);
         }
-
-        for(int i=0; i<n; i++){
-            cout << i << " " << complement[i] << endl;
-        }
-        cout << endl;
 
         int ans = n;
         for(int i=0; i<n; i++){
-            ans = min(ans, n - complement[i] - i);
+            ans = min(ans, n - complement[i]);
         }
         cout << ans << endl;
     }
