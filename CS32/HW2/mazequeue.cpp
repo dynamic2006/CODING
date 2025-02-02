8ecafddfd0006ff25ebf002e13d064bd1fe8eab2
tejasvi <tejasvi.aynor@gmail.com>
2025-02-02 10:59:37 -0800

    Drafted HW2 pts1-4

---
+++ CS32/HW2/mazequeue.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+class Coord
+{
+    public:
+    Coord(int r, int c) : m_row(r), m_col(c) {}
+    int r() const { return m_row; }
+    int c() const { return m_col; }
+    private:
+    int m_row;
+    int m_col;
+};
+
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec)
+{
+    queue<Coord> bfs;
+    bfs.push(Coord(sr, sc)); maze[sr][sc] = 'X';
+    // int counter = 1;
+    while(!bfs.empty()){
+        Coord cur = bfs.front(); bfs.pop();
+        int r = cur.r(), c = cur.c();
+        // cout << counter << ") r: " << r  << " c: " << c << endl; counter++;
+        if(r == er && c == ec) return true; 
+
+        if(r-1 >= 0 && maze[r-1][c] != 'X'){
+            bfs.push(Coord(r-1, c)); maze[r-1][c] = 'X';
+        }
+        if(c+1 < nCols && maze[r][c+1] != 'X'){
+            bfs.push(Coord(r, c+1)); maze[r][c+1] = 'X';
+        }
+        if(r+1 < nRows && maze[r+1][c] != 'X'){
+            bfs.push(Coord(r+1, c)); maze[r+1][c] = 'X';
+        }
+        if(c-1 >= 0 && maze[r][c-1] != 'X'){
+            bfs.push(Coord(r, c-1)); maze[r][c-1] = 'X';
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    string maze[10] = {
+        "XXXXXXXXXX",
+        "X........X",
+        "XX.X.XXXXX",
+        "X..X.X...X",
+        "X..X...X.X",
+        "XXXX.XXX.X",
+        "X.X....XXX",
+        "X..XX.XX.X",
+        "X...X....X",
+        "XXXXXXXXXX"
+    };
+
+    if (pathExists(maze, 10,10, 6,4, 1,1))
+        cout << "Solvable!" << endl;
+    else
+        cout << "Out of luck!" << endl;
+}
