9d78dd9e3dc547f9b6444f92e680f8f245447d27
tejasvi <tejasvi.aynor@gmail.com>
2025-02-05 13:38:49 -0800

    ready to submit mazequeue and mazestack

--- CS32/HW2/mazestack.cpp
+++ CS32/HW2/mazestack.cpp
@@ -18,11 +18,9 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int
 {
     stack<Coord> dfs;
     dfs.push(Coord(sr, sc)); maze[sr][sc] = 'X';
-    // int counter = 1;
     while(!dfs.empty()){
         Coord cur = dfs.top(); dfs.pop();
         int r = cur.r(), c = cur.c();
-        // cout << counter << ") r: " << r  << " c: " << c << endl; counter++;
         if(r == er && c == ec) return true; 
 
         if(r-1 >= 0 && maze[r-1][c] != 'X'){
@@ -39,25 +37,4 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int
         }
     }
     return false;
-}
-
-int main()
-{
-    string maze[10] = {
-        "XXXXXXXXXX",
-        "X........X",
-        "XX.X.XXXXX",
-        "X..X.X...X",
-        "X..X...X.X",
-        "XXXX.XXX.X",
-        "X.X....XXX",
-        "X..XX.XX.X",
-        "X...X....X",
-        "XXXXXXXXXX"
-    };
-
-    if (pathExists(maze, 10,10, 6,4, 1,1))
-        cout << "Solvable!" << endl;
-    else
-        cout << "Out of luck!" << endl;
 }
